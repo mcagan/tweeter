db7a56e809c73842a37dd06d47f5dbe248789ab1
mcagan <meliscagan@gmail.com>
2020-03-24 19:55:47 +0000

    implement counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  console.log("DOM ready");
+  $('textarea').on('keyup', function() {
+    let remainingChars = 140 - $(this).val().length;
+    console.log(remainingChars);
+    if (remainingChars < 0) {
+      $('.counter').css('color', 'red');
+    }
+    $('.counter').text(remainingChars);
+  });
+});
