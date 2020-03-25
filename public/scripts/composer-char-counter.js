09ac356d866830b16b1bea29090f59107386ea46
mcagan <meliscagan@gmail.com>
2020-03-25 16:42:04 +0000

    refactor counter function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
   console.log("DOM ready");
-  $('textarea').on('keyup', function() {
+
+  $("textarea").on("keyup", function() {
+    const $counter = $(this)
+      .parent()
+      .find(".counter");
     let remainingChars = 140 - $(this).val().length;
     console.log(remainingChars);
     if (remainingChars < 0) {
-      $('.counter').css('color', 'red');
+      $counter.css("color", "red");
     } else {
-      $('.counter').css('color', 'black');
+      $counter.css("color", "black");
     }
-    $('.counter').text(remainingChars);
+    $counter.text(remainingChars);
   });
 });
