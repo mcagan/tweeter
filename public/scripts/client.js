be1beb4dbb96f0304bc4845b579e6e2eefa7ffd5
mcagan <meliscagan@gmail.com>
2020-03-25 20:37:59 +0000

    implement ajax get and post requests

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,7 @@
  */
 $(document).ready(function() {
   console.log("DOM ready");
+
   const createTweetElement = function(tweetObj) {
     const $postArticle = $("<article>").addClass("tweet");
     const $header = $("<header>");
@@ -51,35 +52,72 @@ $(document).ready(function() {
     // loops through tweets
     for (let tweet of tweets) {
       let $tweet = createTweetElement(tweet);
-      $("#tweets-container").append($tweet);
+      $("#tweets-container").prepend($tweet);
     }
   };
 
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac"
-      },
-      content: {
-        text:
-          "If I have seen further it is by standing on the shoulders of giants"
-      },
-      created_at: 1461116232227
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd"
-      },
-      content: {
-        text: "Je pense , donc je suis"
-      },
-      created_at: 1461113959088
+  $("#submit").submit(function(event) {
+    event.preventDefault();
+    let data = $(this).serialize();
+    let text = data.split("=");
+    if (text[1].length === 0) {
+      alert("Please enter a tweet");
+    } else if (text[1].length > 140) {
+      alert("Character limit can't be exceeded");
+    } else {
+      $.ajax({ method: "POST", url: "/tweets", data })
+        .done(function(result) {
+          console.log(result);
+        })
+        .fail(function(error) {
+          // Problem with the request
+          console.log(`Error with the request: ${error.message}`);
+        })
+        .always(function() {
+          // This will always run
+          console.log("request completed");
+        });
+      setTimeout(() => {
+        $.ajax({ method: "GET", url: "/tweets" })
+          .done(function(result) {
+            $("#tweets-container").empty();
+            renderTweets(result);
+          })
+          .fail(function(error) {
+            console.log(`Error with the request: ${error.message}`);
+          })
+          .always(function() {
+            console.log("request completed");
+          });
+      }, 1000);
     }
-  ];
+  });
 
-  renderTweets(data);
+  // const data = [
+  //   {
+  //     user: {
+  //       name: "Newton",
+  //       avatars: "https://i.imgur.com/73hZDYK.png",
+  //       handle: "@SirIsaac"
+  //     },
+  //     content: {
+  //       text:
+  //         "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     created_at: 1461116232227
+  //   },
+  //   {
+  //     user: {
+  //       name: "Descartes",
+  //       avatars: "https://i.imgur.com/nlhLi3I.png",
+  //       handle: "@rd"
+  //     },
+  //     content: {
+  //       text: "Je pense , donc je suis"
+  //     },
+  //     created_at: 1461113959088
+  //   }
+  // ];
+
+  // renderTweets(data);
 });
